ad9696cb561d2da9fcffbbcefe7b5bea22212c48
geekfrontend <geekfrontend16@gmail.com>
2024-11-24 18:44:47 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <div className="min-h-screen bg-white artboard phone-3">
+          <header>
+            <h1>Header</h1>
+          </header>
+          <main>
+            <div>
+              <h2>Main</h2>
+            </div>
+          </main>
+          <footer>
+            <h3>Footer</h3>
+          </footer>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4PkKYA17JQgHCoK_CtUH9HaRp_qZwUYE",
+  authDomain: "fir-aplikasi-tugas-akhir.firebaseapp.com",
+  projectId: "fir-aplikasi-tugas-akhir",
+  storageBucket: "fir-aplikasi-tugas-akhir.firebasestorage.app",
+  messagingSenderId: "674782002429",
+  appId: "1:674782002429:web:fddfa1e103e050ad2ad294",
+  measurementId: "G-RWMCFM2QL5"
+};
+
+const app = initializeApp(firebaseConfig)
